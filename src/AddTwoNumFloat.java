b2a2d3fc4478afef36a2157e35a658ea2033605a
Deep100ni <deep100ni13@gmail.com>
2026-05-07 13:39:31 +0530

    AddTwoNumFloat

--- src/AddTwoNumFloat.java
+++ src/AddTwoNumFloat.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class AddTwoNum {
+
+public class AddTwoNumFloat {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number :");
-        int num1 = sc.nextInt();
+        float num1 = sc.nextFloat();
         System.out.println("Enter Second Number :");
-        int num2 = sc.nextInt();
-        int sum = num1 + num2;
+        float num2 = sc.nextFloat();
+        float sum = num1 + num2;
         System.out.println("Sum = "+sum);
     }
 }
